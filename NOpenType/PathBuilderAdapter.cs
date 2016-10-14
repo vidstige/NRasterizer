f6eda72ba1d44a24c05d119905d1f02fe6f4b588
WinterDev <wintercoredev@gmail.com>
2016-10-14 08:47:28 +0200

    Adding GlyphBuilderBase

---
+++ NOpenType/PathBuilderAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NRasterizer
+{
+    // Helperfunctions to make GlyphPathBuilder build
+    public class Point<T>
+    {
+        public readonly T x;
+        public readonly T y;
+
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Point(): this(default(T), default(T))  { }
+
+        public T X { get { return x; } }
+        public T Y { get { return y; } }
+    }
+
+
+    public class FtPoint: Point<float>
+    {
+        //public FtPoint(float x, float y): base(x, y) {}
+    }
+
+    public class FtPointD: Point<double>
+    {
+        public FtPointD() {}
+        public FtPointD(FtPoint p): base(p.X, p.Y) {}
+        public FtPointD(double x, double y): base(x, y) {}
+    }
+
+}
+
