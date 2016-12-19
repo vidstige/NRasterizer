0bc403186319a67d13261b9759a943cfafd42f70
Samuel Carlsson <samuel.carlsson@gmail.com>
2016-12-19 19:50:08 +0100

    Removing defunct Sample project.

--- build/generate_samples.sh
+++ build/generate_samples.sh
@@ -3,7 +3,7 @@
 PROGRAM=NRasterizer.CLI/bin/Release/NRasterizer.CLI.exe
 
 # Generate sample with the GDI+ rasterizer
-mono $PROGRAM gdi+ Sample/segoeui.ttf samples/gdi/cefhijl.png cefhijl
+mono $PROGRAM gdi+ Fonts/segoeui.ttf samples/gdi/cefhijl.png cefhijl
 
 # Generate NRasterizer sample
-mono $PROGRAM nrasterizer Sample/segoeui.ttf samples/clfx.png clfx
+mono $PROGRAM nrasterizer Fonts/segoeui.ttf samples/clfx.png clfx
