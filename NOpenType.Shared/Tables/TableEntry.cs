49b515af6bfd416afafe981b38ec4c93e0ecb1ec
Scott Williams <tocsoft@gmail.com>
2016-12-19 22:47:09 +0000

    move code to shared project
    Created 2 new projects,
    1. a shared one containing all the shared code
    2. a netstandard one targeting netstandard1.1.
    
    updated nuspec to pull in new dll and updated build script so it will pack correctly.

--- NOpenType.Shared/Tables/TableEntry.cs
+++ NOpenType.Shared/Tables/TableEntry.cs
@@ -40,7 +40,7 @@ namespace NRasterizer.Tables
         {
             var bytes = BitConverter.GetBytes(tag);
             Array.Reverse(bytes);
-            return Encoding.ASCII.GetString(bytes);
+            return Encoding.Unicode.GetString(bytes, 0, bytes.Length);
         }
 
         public override string ToString()
