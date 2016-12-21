107d11cd9cd8d113be8bae40a392b6e91c3f9577
Scott Williams <tocsoft@gmail.com>
2016-12-21 12:04:17 +0000

    added font loading tests

---
+++ NRasterizer.Tests/OpenTypeReaderTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRasterizer.Tests
+{
+
+    [TestFixture]
+    public class OpenTypeReaderTests
+    {
+        [Test]
+        [TestCase("CompositeMS.ttf")]
+        [TestCase("segoeui.ttf")]
+        [TestCase("OpenSans-Regular.ttf")]
+        public void LoadingFontDoesntThrowAnyExceptions(string fontFilename)
+        {
+            var reader = new OpenTypeReader();
+            Assert.DoesNotThrow(() =>
+            {
+                using (var fs = File.OpenRead($"TestFonts/{fontFilename}"))
+                {
+                    var typeface = reader.Read(fs);
+                }
+            });
+        }
+    }
+}
