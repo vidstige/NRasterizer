7d8740579ca873523dace408ac96c191bebcba27
Samuel Carlsson <samuel.carlsson@gmail.com>
2016-12-26 09:14:35 +0100

    Build: Using `dotnet run` for samples

--- build/generate_samples.cmd
+++ build/generate_samples.cmd
@@ -1,16 +1,9 @@
 @ECHO off
-SET cli=NRasterizer.CLI\bin\Release\net45\NRasterizer.CLI.exe
-ECHO %cli%
-ECHO %cd%
+SET cli=dotnet run --project NRasterizer.CLI/ --configuration Release
 
-DIR NRasterizer.CLI\bin
-DIR NRasterizer.CLI\bin\Release
-DIR NRasterizer.CLI\bin\Release\net45
-
+REM Generate samples with the GDI+ rasterizer
+ECHO Rendering samples with GDI+
 %cli% gdi+ Fonts\CompositeMS.ttf samples\C.png C
-
-ECHO Rendering sample with GDI+
-REM Generate sample with the GDI+ rasterizer
 %cli% gdi+ Fonts\segoeui.ttf samples\gdi\cefhijl.png cefhijl
 
 ECHO Rendering sample with nrasterizer
